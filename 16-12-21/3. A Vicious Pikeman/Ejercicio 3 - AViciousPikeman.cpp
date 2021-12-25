efc39ecc922a6abcbbe93760aac0daee944aad63
Fernanda Paredes <fparedese@unsa.edu.pe>
2021-12-25 18:03:01 -0500

    A Vicious Pikeman

---
+++ 16-12-21/3. A Vicious Pikeman/Ejercicio 3 - AViciousPikeman.cpp
@@ -0,0 +1,44 @@
+// Ejercicio 3: A Vicious Pikeman
+// URL: https://open.kattis.com/problems/pikemaneasy
+// Curso: Programación Competitiva
+// Grupo: A
+// Alumna: Fernanda Anthoanette Paredes Escobedo
+// CUI: 20182906
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int comp(const void *p, const void *q) {
+  int *a = (int *) p;
+  int *b = (int *) q;
+  return *a - *b;
+}
+
+int main() {
+  long n, t, a, b, c, p, s, i, t_p;
+  long ti[10000];
+
+  cin >> n >> t >> a >> b >> c >> ti[0];
+
+  for ( i = 1; i < n; i++ )
+    ti[i] = (a * ti[i-1] + b) % c + 1;
+
+  qsort(ti, n, sizeof(long), comp);
+
+  p = 0;
+  s = 0;
+  t_p = 0;
+  for (i = 0; i < n; i++) {
+    if (t_p + ti[i] <= t) {
+      p = (p + ti[i] + t_p) % 1000000007;
+      t_p += ti[i];
+      s++;
+    } else {
+      break;
+    }
+  }
+
+  cout << s << " " << p << endl;
+  
+  return 0;
+}
